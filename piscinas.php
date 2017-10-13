56b7fe8bff56e3561a04a6b4205ec23e561380bd
AlexisGnz <Alexisgonzalezemmanuel@hotmail.com>
2017-10-13 03:44:32 -0300

    piscinas slider complete

--- piscinas.php
+++ piscinas.php
@@ -7,6 +7,11 @@
 <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
 <script src="https://use.fontawesome.com/a3901a9fed.js"></script>
 <link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="js/jquery.bxslider/jquery.bxslider.css">
+<!-- Owl Stylesheets -->
+<link rel="stylesheet" href="js/owlcarousel/assets/owl.carousel.min.css">
+<link rel="stylesheet" href="js/owlcarousel/assets/owl.theme.default.min.css">
+
 </head>
 <body>
 <div class="header">
@@ -80,6 +85,16 @@
     </div>            
   </div>
 </div>
+  <div class="inner_page">
+    <div class="container">
+      <h2>¿Cómo trabajamos?</h2>
+      <div class="menu_slider_empresa"><ul class="clearfix"><li><a>DISEÑO & ESTRUCTURA</a></li><li><a>ESTANQUEIDAD DE LA PISCINA</a></li><li><a>DETALLES & TERMINACIONES</a></li></ul></div>
+      <div class="slider_empresa slider-piscinas"><ul><li><img src="img/piscinas-slider-1.png" alt=""><p><a href="">Diseño de la piscina: </a>Forma, color, medida, profundidad, materiales y presupuesto. Asesoramiento profesional personalizado para el diseño ideal.</p><p><a href="">Estudio del terreneno: </a>Visitamos el terreno para seleccionar juntos el lugar idóneo para la ubicación de la piscina.</p><p><a href="">Preparación del terreno: </a>Excavación de la zona, explanación y colocación de solera de hormigón con las profundidades elegidas por el cliente.</p><p><a href="">Comienzo de obra: </a>Instalación y construcción de la estructura principal que aporta la resistencia necesaria para soportar la presión del agua y sus accesos. </p></li>
+<li><img src="img/piscinas-slider-2.png" alt=""><p><a href="">Uniformidad de las paredes: </a>Cercamos las paredes con madera y acero reforzado. Controlamos que queden perfectamente alineadas unas con otras.</p><p><a href="">Impermeabilización y estanqueidad: </a>Colocación de membrana impermeable y flexible; adaptable a posibles movimientos del terreno.</p><p><a href="">Instalaciones: </a>Colocación de las tuberías, del sistema de suministro y filtración; y del sistema eléctrico para las luces.</p><p><a href="">Construcción: </a>Cimentación del suelo y de las paredes. Terminación y revestimiento impermeable de la piscina.</p></li>
+<li><img src="img/piscinas-slider-3.png" alt=""><p><a href="">Relleno del exterior: </a>Relleno del espacio posterior de las paredes de la piscina. Construcción de borde perimetral, solarium y deck.</p><p><a href="">Terminaciones: </a>Pintura o revestimientos especiales, colocación de accesorios: hidromasajes, luces subacuáticas, cascadas, etc. y control de detalles estéticos.</p><p><a href="">Puesta en funcionamiento: </a>Llenado y puesta en marcha de la piscina para comprobar su correcta impermeabilización y desague. Conexión eléctrica.</p><p><a href="">Capacitación Final: </a>Asesoramiento respecto al correcto uso de la bomba y provisión de productos para el tratamiento y mantenimiento del agua.</p></li>
+</ul></div>
+  </div>
+</div>
 <div class="footer">
   <div class="newsletters_block">
     <div class="container">
@@ -150,5 +165,9 @@
     </div>
   </div>
 </div>
+<script src="js/jquery.bxslider/jquery.bxslider.js"></script>
+<script src="js/owlcarousel/owl.carousel.js"></script>
+
+<script src="js/scripts.js"></script>
 </body>
 </html>
