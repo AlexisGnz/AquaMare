abb7473ae573fc399ee66cad08140beb0e97e12a
AlexisGnz <Alexisgonzalezemmanuel@hotmail.com>
2017-10-21 14:47:19 -0300

    Energy page

--- energias.php
+++ energias.php
@@ -41,6 +41,119 @@
   </div>
 </div>
 <img class="img-responsive" src="img/energias.png">
+<div class="darkness-container clearfix">
+  <div class="container clearfix">
+    <div class="darkness-header text-center">
+      <h2>El futuro energético</h2>
+      <strong>ESTAMOS PARA BRINDARLE LAS MEJORES OPCIONES EN ENERGIA RENOVABLE</strong>
+      <p>El cuidado del medio ambiente es fundamental para nuestra empresa.
+Aquamare trabaja de manera constante e intensa en el desarrollo y la comercialización de equipamientos y productos
+que contribuyan a mejorar el mundo en el que vivimos. Para ello te brindamos las mejores opciones con respecto
+a la utilización de energía renovable que nos permite ahorrar en consumo de energía. 
+</p>
+    </div>
+    <div class="darkness-stick-container">
+      <div class="col-md-4 col-fix-darkness"><img src="img/stick-energias-1.png" alt=""></div>
+      <div class="col-md-8 col-fix-darkness">
+        <h3 class="bar-energias"><strong>TERMOTANQUE A ENERGIA SOLAR: </strong>Ideal para el calentamiento de agua</h3>
+        <section>
+          <p>Es un sistema que no consume combustible. 
+Funciona con captadores de tubos al vacío que se basan en el principio de HEAT-PIPE, 
+es decir, un equipo que transforma la energía solar en energía térmica. Está compuesto por 
+dos elementos principales: un depósito de almacenamiento y un colector solar.</p>
+          <p>En viviendas puede servir como la única fuente de agua caliente. También se lo puede 
+incorporar a viviendad que ya cuenten con un calefón o termotanque instalado. </p>
+          <strong><a href="">Ventajas: </a>son totalmente autónomos. Su mantenimiento es mínimo. El ahorro de energía es de un 70%. 
+Alto rendimiento por su sistema Heat-Pipe (calienta pero no quema) los tubos alcanzan una temperatura límitada, 
+lo que protege al sistema de sobrecalentamientos.
+</strong>
+        </section>
+      </div>
+    </div>
+
+    <div class="darkness-stick-container">
+      <div class="col-md-4 col-fix-darkness float-right"><img src="img/stick-energias-2.png" alt=""></div>
+      <div class="col-md-8 col-fix-darkness">
+        <h3 class="bar-energias"><strong>ENERGIA FOTOVOLTAICA: </strong>Ideal para abastecer cualquier tipo de red de distribución eléctrica.</h3>
+        <section>
+          <p>La energía fotovoltáica es un tipo de energía renovable que se obtiene de los rayos 
+del sol gracias a la foto-detección cuántica de un dispositivo. Normalmente se utiliza 
+una lámina semi-conductora llamada célula fotovoltáica.  </p>
+          <p>Los paneles fotovoltáicos transforman la radiación solar en energía eléctrica y la almacenan 
+en baterías. Son modulares, pudiendo agregarse más según las necesidades de potencia.
+Excelente opción para abastecer innumerables aparatos eléctricos y viviendas aisladas.</p>
+          <strong><a href="">Múltiples Ventajas: </a>Instalación simple y adaptable a diversas necesidades energéticas.
+No consume combustible. No es contaminante. No requiere mantenimiento.
+Sus celdas duran décadas.</strong>
+        </section>
+      </div>
+    </div>
+
+    <div class="darkness-stick-container">
+      <div class="col-md-4 col-fix-darkness"><img src="img/stick-energias-3.png" alt=""></div>
+      <div class="col-md-8 col-fix-darkness">
+        <h3 class="bar-energias"><strong>ILUMINACION SOLAR: </strong>La más confiable inversión a largo plazo.</h3>
+        <section>
+          <p>Contamos con artefactos lumínicos que contienen su propio panel solar que absorbe 
+la energía del sol y la acumula en una batería recargable que se activa de noche. 
+Hay modelos para parques, jardínes, senderos, decks, calles, estacionamientos o cualquier 
+sector exterior donde no llegue la red eléctrica.</p>
+          <p>Estos artefactos son autónomos. Se encienden solos cuando comienza a anochecer y se 
+apagan automáticamente cuando amanece. También se los puede manejar de forma manual.</p>
+          <strong><a href="">Ventajas: </a>Bajo costo y gran aprovechamiento. 
+Fácil de instalar. No requieren cables. Ahorra energía.
+Contará con energía gratuita de por vida. Los artefactos son seguros para los niños y mascotas.
+</strong>
+        </section>
+      </div>
+    </div>
+
+    <div class="darkness-stick-container">
+      <div class="col-md-4 col-fix-darkness float-right"><img src="img/stick-energias-4.png" alt=""></div>
+      <div class="col-md-8 col-fix-darkness">
+        <h3 class="bar-energias"><strong>COLECTOR SOLAR PARA CLIMATIZACION: </strong>Ideal para el calentamiento de su piscina.</h3>
+        <section>
+          <p>El colector solar utiliza el sistema de filtrado existente de su piscina. Es de apariencia
+discreta y diseño sobrio. Va colocado sobre el techo. Resulta muy liviano y adecuado para
+todo tipo de instalaciones. Está fabricado de polipropileno. Los tubos y cabezales forman
+una pieza única. Es de color negro integral por lo que absorbe mucho el calor.</p>
+          <p>Esta realizado en un material flexible e irrompible. Es de baja presión de trabajo
+y transfiere el calor óptimamente debido al amplio área de contacto.</p>
+          <strong><a hrenergiasVentajas: </a>Resistente a los rayos UV. No se despinta con el tiempo. Alta resistencia a la interperie.
+Alta absorción de energía solar. Alta eficiencia térmica. Instalación sencilla.
+Mantenimiento nulo y expectativa de vida mayor a 20 años.
+Sus celdas duran décadas.</strong>
+        </section>
+      </div>
+    </div>
+
+
+    <div class="btn-containere"><button>necesito un presupuesto</button></div>          
+    </div>
+  </div>
+    <div class="piscinas-contacto background-energias">
+  <section>
+  <h2 class="color-energias">Estas alternativas usan al sol como principal fuente de energía
+que permite el aprovechamiento de la radiación que emite</h2>
+  <h3>¿Necesitás energía eléctrica rentable?</h3>
+  <button class="btn-energias">contactanos</button></section>
+</div>
+<div class="jobs-piscinas text-center clearfix container">
+  <img src="img/icono-energia.png" alt="">
+  <h2>Ver los trabajos realizados</h2>
+  <button>quiero mas info</button>
+  <br>
+</div>
+<div class="sponsors-section clearfix text-center container">
+  <div class="col-md-2"><img src="img/sponsors-1.png" alt=""></div>
+  <div class="col-md-2"><img src="img/sponsors-2.png" alt=""></div>
+  <div class="col-md-2"><img src="img/sponsors-3.png" alt=""></div>
+  <div class="col-md-2"><img src="img/sponsors-4.png" alt=""></div>
+  <div class="col-md-2"><img src="img/sponsors-5.png" alt=""></div>
+  <div class="col-md-2"><img src="img/sponsors-6.png" alt=""></div>
+</div>
+</div>
+
 <div class="footer">
   <div class="newsletters_block">
     <div class="container">
